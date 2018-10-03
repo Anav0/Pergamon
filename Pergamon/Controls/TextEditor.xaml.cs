34899007563b2cd360e42d80abf5abc7042b9885
Anav <janbul6@gmail.com>
2018-10-03 19:30:44 +0200

    Added ability to change position of a action bar

--- Pergamon/Controls/TextEditor.xaml.cs
+++ Pergamon/Controls/TextEditor.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Pergamon
 {
@@ -10,6 +12,47 @@ namespace Pergamon
         public TextEditor()
         {
             InitializeComponent();
+            DataContext = new TextEditorViewModel();
         }
+
+        #region BarBackground
+
+        public Brush BarBackground
+        {
+            get { return (Brush)GetValue(BarBackgroundProperty); }
+            set { SetValue(BarBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty BarBackgroundProperty =
+            DependencyProperty.Register("BarBackground", typeof(Brush), typeof(TextEditor), new PropertyMetadata(new Control().FindResource("DefaultTabBackgroundColorBrush")));
+
+        #endregion
+
+        #region BarForeground
+
+        public Brush BarForeground
+        {
+            get { return (Brush)GetValue(BarForegroundProperty); }
+            set { SetValue(BarForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty BarForegroundProperty =
+            DependencyProperty.Register("BarForeground", typeof(Brush), typeof(TextEditor), new PropertyMetadata(new Control().FindResource("DefaultTabForegroundColorBrush")));
+
+        #endregion
+
+        #region BarPlacement
+
+        public Dock BarPlacement
+        {
+            get { return (Dock)GetValue(BarPlacementProperty); }
+            set { SetValue(BarPlacementProperty, value); }
+        }
+
+        public static readonly DependencyProperty BarPlacementProperty =
+            DependencyProperty.Register("BarPlacement", typeof(Dock), typeof(TextEditor), new PropertyMetadata(Dock.Top));
+
+        #endregion
+
     }
 }
---
+++ Pergamon/ViewModels/TextEditorViewModel.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Windows.Input;
+
+namespace Pergamon
+{
+    public class TextEditorViewModel : BaseViewModel
+    {
+        #region Public properties
+
+
+
+        #endregion
+
+
+        public TextEditorViewModel()
+        {
+            ShowFontColorPickerCommand = new RelayCommand(ShowFontColorPicker);
+            ShowMarkerColorPickerCommand = new RelayCommand(ShowMarkerColorPicker);
+
+        }
+
+        #region Command Methods
+
+        private void ShowMarkerColorPicker()
+        {
+        }
+
+        private void ShowFontColorPicker()
+        {
+        }
+
+        #endregion
+
+        #region Public Commands
+
+        public ICommand ShowFontColorPickerCommand { get; set; }
+
+        public ICommand ShowMarkerColorPickerCommand { get; set; }
+
+        #endregion
+    }
+}
